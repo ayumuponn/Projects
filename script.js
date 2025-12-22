f3db2333eb750da9245f33d8ffe5583bb75cebde
Ayaka Arakane <250960426+ayumuponn@users.noreply.github.com>
2025-12-22 22:02:39 +0900

    ドキュメントのフォーマットを適用

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function checkCopyright() { //著作権判定
     const deathYear = document.querySelector(`#deathYearInput`).value;
     const resultArea = document.querySelector(`#resultArea`);
 
-    resultArea.className =``; //入力欄消去（空欄で上書き）
+    resultArea.className = ``; //入力欄消去（空欄で上書き）
 
-    if(!deathYear || isNaN(deathYear)) {
+    if (!deathYear || isNaN(deathYear)) {
         resultArea.innerHTML = "無効、没年を正しく入力してください";
         return;
     }
@@ -14,20 +14,20 @@ function checkCopyright() { //著作権判定
     const expiryYear = parseInt(deathYear) + PROTECTION_YEARS; //失効70年（2018年12月31日以前は50年）
     const publicDomainYear = expiryYear + 1; //※パブリックドメインになるのは著作権消滅翌年の1月1日
 
-    let message ="";
+    let message = "";
     if (currentYear >= publicDomainYear) {
-        message =`パブリックドメイン（著作権消滅済）`;
+        message = `パブリックドメイン（著作権消滅済）`;
         message += `<br>著作権は${expiryYear}年末に消滅しました。`;
         //著作権消滅の緑色適用（CSS上書き）
         resultArea.className = `copyright-expired`
-        } else { //著作権失効までの年（yearRemaining）
-            const yearsRemaining = publicDomainYear - currentYear;
-            message = `著作権保護期間内です（著作権有効）`
-            message += `<br>残りおおよそ${yearsRemaining}年です。`;
+    } else { //著作権失効までの年（yearRemaining）
+        const yearsRemaining = publicDomainYear - currentYear;
+        message = `著作権保護期間内です（著作権有効）`
+        message += `<br>残りおおよそ${yearsRemaining}年です。`;
         //著作権有効中の赤色適用（CSS上書き）
         resultArea.className = `copyright-valid`
-        }
+    }
 
-        //結果をHTMLに入力
-        resultArea.innerHTML = message;
+    //結果をHTMLに入力
+    resultArea.innerHTML = message;
 }
